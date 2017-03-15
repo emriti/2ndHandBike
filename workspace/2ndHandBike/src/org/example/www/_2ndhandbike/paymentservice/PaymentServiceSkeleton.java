9e384577b2fd4ad69876981dc10311ea968edc25
ahmad fajri <emriti@gmail.com>
2017-03-15 09:47:06 +0100

    init src

---
+++ workspace/2ndHandBike/src/org/example/www/_2ndhandbike/paymentservice/PaymentServiceSkeleton.java
@@ -0,0 +1,32 @@
+
+/**
+ * PaymentServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.4  Built on : Dec 28, 2015 (10:03:39 GMT)
+ */
+    package org.example.www._2ndhandbike.paymentservice;
+    /**
+     *  PaymentServiceSkeleton java skeleton for the axisService
+     */
+    public class PaymentServiceSkeleton{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param payment 
+             * @return paymentResponse 
+         */
+        
+                 public org.example.www._2ndhandbike.paymentservice.PaymentResponse payment
+                  (
+                  org.example.www._2ndhandbike.paymentservice.Payment payment
+                  )
+            {
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#payment");
+        }
+     
+    }
+    
